0a928ebc3bc847bd8adb6ff1a05e8457d0ea8877
DeoDurant <choieanson@hotmail.com>
2023-03-28 15:18:10 -0500

    Added filter state but no functionality yet

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,10 +1,19 @@
+import React, { useState } from "react"
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card"
 import "./Expense.css";
+import ExpensesFilter from "./ExpensesFilter"
 
 function Expense(props) {
+  const [filteredYear, setFilteredYear] = useState('2020')
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
   return (
-    <Card className="expenses">
+    <div>
+      <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -26,6 +35,9 @@ function Expense(props) {
         date={props.expenses[3].date}
       />
     </Card>
+
+    </div>
+    
   );
 }
 
