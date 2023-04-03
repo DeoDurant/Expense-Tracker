2c2afb6242aafba1fb353a9bde6d22891aec7718
DeoDurant <choieanson@hotmail.com>
2023-04-03 17:16:42 -0500

    The list is now dynamic and a user is now able to add an expense

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,43 +1,32 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card"
+import Card from "../UI/Card";
 import "./Expense.css";
-import ExpensesFilter from "./ExpensesFilter"
+import ExpensesFilter from "./ExpensesFilter";
 
-function Expense(props) {
-  const [filteredYear, setFilteredYear] = useState('2020')
+const Expense = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
+  };
+
   return (
     <div>
       <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
-
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.expenses?.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
     </div>
-    
   );
 }
 
