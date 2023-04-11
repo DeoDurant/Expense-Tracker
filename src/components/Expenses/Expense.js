f275bf188532f9f6ffc85ab1e67b39e0b412030a
DeoDurant <choieanson@hotmail.com>
2023-04-11 18:02:16 -0500

    Added another way to do conditional content by creating the condition within the function.

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -15,6 +15,21 @@ const Expense = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  // Multiple ways to do conditional content.
+  
+  let expensesContent = <p>No expenses found. </p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -22,7 +37,8 @@ const Expense = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.length === 0 && <p>No expenses found. </p>}
+        {expensesContent}
+        {/* {filteredExpenses.length === 0 && <p>No expenses found. </p>}
         {filteredExpenses.length > 0 &&
           filteredExpenses.map((expense) => (
             <ExpenseItem
@@ -31,7 +47,7 @@ const Expense = (props) => {
               amount={expense.amount}
               date={expense.date}
             />
-          ))}
+          ))} */}
       </Card>
     </div>
   );
